ba2d6a108f4026afc06a9c901baab3ae315c1f13
Peterson <pepe419@gmail.com>
2021-01-24 04:09:48 -0300

    Criado a funcionalidade para cadastrar usuarios

--- app/src/main/java/com/example/gerenciarjogos/ui/CadastroUsuariosActivity.kt
+++ app/src/main/java/com/example/gerenciarjogos/ui/CadastroUsuariosActivity.kt
@@ -7,124 +7,47 @@ import com.example.gerenciarjogos.R
 import com.example.gerenciarjogos.model.Usuario
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_cadastro_usuarios.*
 
 class CadastroUsuariosActivity : AppCompatActivity() {
- //   private lateinit var  database: FirebaseFirestore
- //   private lateinit var  myRef: CollectionReference
-
-    //private lateinit var  database: FirebaseDatabase
-    //private lateinit var  myRef: DatabaseReference
+    private lateinit var  database: FirebaseDatabase
+    private lateinit var  myRef: DatabaseReference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //val mStorageRef: StorageReference
-        //mStorageRef = FirebaseStorage.getInstance().getReference();
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_usuarios)
 
-
+        config()
 
         btnCriarAccount.setOnClickListener{
-
-            config()
-            var usuario = getUsuario()
-            sendUsuario(usuario)
-
-            Log.i("TESTE", "Antes de pegar os dados")
             var usario = getUsuario()
-
-            Log.i("TESTE", "Antes de enviar os dados")
             sendUsuario(usario)
-            Log.i("TESTE", "Depois de enviar os dados")
         }
     }
 
     fun config(){
-        fun config(){
-            //database = FirebaseDatabase.getInstance()
-            //myRef = database.reference
-
-            database = FirebaseFirestore.getInstance()
-            myRef = database.collection("usuario")
-        }
+         database = Firebase.database
+         myRef = database.getReference("usuarios/user")
     }
 
-
-
     fun getUsuario(): Usuario {
-        //val usuario2: MutableMap<String, Any> = HashMap()
-        //val usuario: Usuario(txtName.text.toString(); txtEmail.text.toString(); txtPassword.text.toString(); txtPasswordRepeat.text.toString())
-        val usuario = Usuario(
+      val usuario = Usuario(
             txtName.text.toString(),
             txtEmail.text.toString(),
             txtPassword.text.toString(),
             txtPasswordRepeat.text.toString()
-        )
-
-        Log.i("USUARIO", usuario.name)
-        Log.i("USUARIO", usuario.email)
-        Log.i("USUARIO", usuario.senha)
-        Log.i("USUARIO", usuario.senhaConfirmacao)
-
-       // val usuario: Usuario
-
-        //usuario.name = txtName.text.toString()
-        //txtEmail.text.toString()
-        //txtPassword.text.toString()
-        //txtPasswordRepeat.text.toString()
-
-
+      )
         return usuario
-
-
-
-
-
     }
-
 
     fun sendUsuario(user: Usuario){
-
-       // val nome = edNomeProd.text.toString()
-
-       // myRef.child("usuario").setValue(user)
-        myRef.document("usuario").set(user)
-
-       /*     .set(prod).addOnSuccessListener {
-            Log.i(TAG, it.toString())
-
-        }.addOnFailureListener{
-            Log.i(TAG, it.toString())
-        }*/
+        myRef.setValue(user)
     }
-
-
-
-
-
-
-
-
-
-
-
-
-    fun getData(): MutableMap<String, Any>{
-        val prod: MutableMap<String, Any> = HashMap()
-
-
-        prod["Nome"] = "Sabonete"
-        prod["qtd"] = "1"
-        prod["preco"] = "2"
-
-        return prod
-
-    }
-
 
 }
 
